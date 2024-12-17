0b0b040f0e8ded6a6f60cfe790b2a64e33231fe7
limon <limonsk026@gmail.com>
2024-12-17 19:47:10 +0600

    create api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // use middle ware
 app.use(cors());
@@ -17,9 +17,9 @@ app.get("/", (req, res) => {
 // DB_USER = job-hunter
 // DB_PASS = TI4tdvVJbQMV7sit
 
-// server side connect to mongodb 
+// server side connect to mongodb
 
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.h3mkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h3mkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -36,20 +36,64 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // Job portal related apis
+    const jobsCollection = client.db("jobPortal").collection("jobs");
+    const jobApplicationCollection = client
+      .db("jobPortal")
+      .collection("job_applications");
+
+    // get all job to database
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get specifide job to database
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Job applicant related apis
+    app.get("/job-application", async (req, res) => {
+      const email = req.query.email;
+      const query = { applcant_email: email };
+      const result = await jobApplicationCollection.find(query).toArray();
+
+      // fokira way
+      for (const application of result) {
+        console.log(application.job_id);
+        const query1 = {_id: new ObjectId(application.job_id)};
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
+          application.title = job.title;
+          application.company_logo = job.company_logo;
+          application.company = job.company;
+          application.location = job.location;
+        }
+      }
+
+      res.send(result);
+    });
+
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
 
 // create a listener
 app.listen(port, () => {
